dd52cd9c3fc900334219d5944c63d83ec6912b0e
Liliya <charlyrsmail@gmail.com>
2022-02-23 17:58:56 +0300

    done

--- Vector/Vector.h
+++ Vector/Vector.h
@@ -9,16 +9,16 @@
 #endif
 
 
-class Vector {
+class VECTOR_API Vector {
 private:
 	Number x;
 	Number y;
 public:
-	VECTOR_API Vector();
-	VECTOR_API Vector(Number x, Number y);
-	VECTOR_API Number calculatePolarRadius();
-	VECTOR_API Number calculatePolarAngle();
-	VECTOR_API Vector operator+(const Vector& vector);
+	Vector();
+	Vector(Number x, Number y);
+	Number calculatePolarRadius();
+	Number calculatePolarAngle();
+	Vector operator+(const Vector& vector);
 };
 
 const Vector VECTOR_ZERO = Vector(Number(0), Number(0));
