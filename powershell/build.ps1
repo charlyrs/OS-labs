20dc43327a960541fce448842363f390b946461d
Liliya <charlyrsmail@gmail.com>
2022-03-09 18:29:15 +0300

    win

--- powershell/build.ps1
+++ powershell/build.ps1
@@ -1,17 +1,18 @@
 mkdir build
 echo  [ Building Number... ]
 
-g++ -c Number/Number.cpp -o build/Number.exe
-ar rc build/Number.lib build/Number.exe
+g++ -c Number/Number.cpp -o build/Number.o
+ar rc build/Number.lib build/Number.o
 
 echo [ Building Vector... ]
 
-g++ -c -INumber Vector/Vector.cpp -o build/Vector.exe
-g++  build/Vector.exe build/Number.lib -shared -o build/Vector.dll
+g++ -c -INumber Vector/Vector.cpp -o build/Vector.o
+g++  build/Vector.o build/Number.lib -shared -o build/Vector.dll
 
 echo [ Building Main... ]
 
 g++ -o lab1.exe lab1console/lab1.cpp -INumber -IVector build/Vector.dll build/Number.lib;
 .\lab1.exe
-
+echo [FINISHED]
+rm -R build
 
